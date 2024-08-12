ecfc0e161fc73563a5a3a74e12341a7f5867e4e4
Ellie <ellieslife@icloud.com>
2024-08-12 16:23:26 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,5 @@
 const { PiecesClient } = require('pieces-copilot-sdk');
 
-// Make sure the baseUrl is your actual base URL
 const piecesClient = new PiecesClient({ baseUrl: 'http://localhost:1000' });
 
 async function askQuestion() {
